f81a46d71d6a4e557849526432b76440f5daa92f
Brennan Seymour <brenno263@gmail.com>
2024-09-13 19:03:49 -0500

    fix typos, add countdown, add text animation

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,55 @@
+import classes from "./Countdown.module.css";
+import { createComputed, createMemo, createSignal, onCleanup, onMount } from "solid-js";
+
+const RELEASE_DATE = new Date("October 21, 2024 00:00:00");
+
+const DAY = 1000 * 60 * 60 * 24;
+const HOUR = 1000 * 60 * 60;
+const MINUTE = 1000 * 60;
+const SECOND = 1000;
+
+export function Countdown(props: {releaseDateString: string}) {
+    const releaseDateMs = createMemo(() => {
+        console.log("foo");
+        return Number(new Date(props.releaseDateString));
+    });
+    const getTimeLeft = () => {
+        return releaseDateMs() - Number(new Date());
+    }
+
+    const [ms, setMs] = createSignal(getTimeLeft());
+
+    const msToDays = (ms: number) => Math.floor(ms / DAY);
+    const msToHours = (ms: number) => Math.floor((ms % DAY) / HOUR);
+    const msToMinutes = (ms: number) => Math.floor((ms % HOUR) / MINUTE);
+    const msToSeconds = (ms: number) => Math.floor((ms % MINUTE) / SECOND);
+
+    const leftPadNumber = (n: number, digits: number) => {
+        let str = String(n);
+        while (str.length < digits) {
+            str = "0" + str;
+        }
+        return str;
+    }
+    
+
+    onMount(() => {
+        let animationFrame = requestAnimationFrame(updateAnimation);
+        function updateAnimation() {
+        setMs(getTimeLeft());
+        animationFrame = requestAnimationFrame(updateAnimation);
+        }
+
+        onCleanup(() => {
+            cancelAnimationFrame(animationFrame);
+        })
+    })
+
+
+    return (
+        <div class={classes["container"]}>
+            <p class={classes.number}>{msToDays(ms())}:{leftPadNumber(msToHours(ms()), 2)}:{leftPadNumber(msToMinutes(ms()), 2)}:{leftPadNumber(msToSeconds(ms()), 2)}:{leftPadNumber(ms() % 1000, 3)}</p>
+            
+        </div>
+    );
+}
